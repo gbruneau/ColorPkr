4697039e677c040f1e6a512f2fc1594db47525db
gbruneau <guy.bruneau@gmail.com>
2022-11-11 08:01:40 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -219,7 +219,7 @@ function setTextBoxColor(id,fg,bg){
 function setTextContent(){
   var allText=document.querySelectorAll(".textColorBox")
   for (const aText of allText){
-    aText.innerText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. 🭁🭌"
+    aText.innerText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. ■■"
   }
 }
 
